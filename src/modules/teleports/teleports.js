262c24a087ca339da2e4c639b590715f8ade29be
hurfy <hurfydev@gmail.com>
2024-01-16 13:26:01 +0300

    Optimized teleport function

--- src/modules/teleports/teleports.js
+++ src/modules/teleports/teleports.js
@@ -3,34 +3,12 @@ const teleportsData = require("./teleportsData");
 console.log("[GAMEMODE|MODULES] teleports.js loaded!");
 
 const teleport = function (player, args) {
-	const playerVehicle = player.getVehicle();
+	const target = player.getVehicle() || player;
+	const spot = /[a-zA-Z]/.test(args)
+		? teleportsData.teleports[args]
+		: sdk.Vector3.apply(null, args.slice(0, 3).map(parseFloat));
 
-	const isStringSpot = /[a-zA-Z]/.test(args);
-	const isNumericCoords = args.length >= 3;
-
-	if (isStringSpot || isNumericCoords) {
-		const spots = teleportsData.teleports;
-
-		if (playerVehicle) {
-			if (isStringSpot) {
-				// player.sendChat("TP/VEHICLE");
-
-				playerVehicle.setPosition(spots[args]);
-			} else if (isNumericCoords) {
-				const [x, y, z] = args.slice(0, 3).map(parseFloat);
-				playerVehicle.setPosition(sdk.Vector3(x, y, z));
-			}
-		} else {
-			if (isStringSpot) {
-				// player.sendChat("TP/PLAYER");
-
-				player.setPosition(spots[args]);
-			} else if (isNumericCoords) {
-				const [x, y, z] = args.slice(0, 3).map(parseFloat);
-				player.setPosition(sdk.Vector3(x, y, z));
-			}
-		}
-	}
+	target.setPosition(spot);
 };
 
 module.exports = {
